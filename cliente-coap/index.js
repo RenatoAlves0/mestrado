581496727d4ecaf3a0e7faed2304117b3b5edabb
RenatoAlves0 <renatoalves97.123@gmail.com>
2022-07-31 12:56:05 -0300

    new message

--- cliente-coap/index.js
+++ cliente-coap/index.js
@@ -7,7 +7,7 @@ enviar_msg = (dados) => {
             port: 5683,
             pathname: "/presence",
             method: 'post',
-            confirmable: 'true'
+            confirmable: 'false'
         })
     cliente.write(dados)
     cliente.end()
@@ -17,7 +17,7 @@ for (let i = 1; i <= 1000; i++) {
     if (i == 1) console.log("1° Msg: ", new Date())
     if (i == 1000) console.log("Última Msg: ", new Date())
     if (i != 1000)
-        enviar_msg('hello')
+        enviar_msg('ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789')
     if (i == 1000)
         enviar_msg('end')
 }
