1aef45374e2f4be55f23ae71af7a22d5c2aaf199
RenatoAlves0 <renatoalves97.123@gmail.com>
2023-01-03 19:23:55 -0300

    Alterações no service

--- services/index.js
+++ services/index.js
@@ -1,3 +1,5 @@
+// const fs = require('fs')
+
 const ideal = {
     t: 20,
     u: 75,
@@ -6,25 +8,49 @@ const ideal = {
     l: 70
 }
 
+const alertas = []
+
 export const check = (props, type, cpu, mem, totalMsg, firstMsg) => {
     const value = JSON.parse(props.toString())
 
     if (type == 'agro') {
-        console.log(value.telemetry[0].l.t != ideal.t && 'Temperatura está fora da faixa ideal')
-        console.log(value.telemetry[0].l.u != ideal.u && 'Umidade está fora da faixa ideal')
-        console.log(value.telemetry[0].l.us != ideal.us && 'Umidade do solo está fora da faixa ideal')
-        console.log(value.telemetry[0].l.c != ideal.c && 'Nível de Chuva está fora da faixa ideal')
-        console.log(value.telemetry[0].l.l != ideal.l && 'Nível de Luz está fora da faixa ideal')
+        value.telemetry.forEach(el => {
+            el.l.t != ideal.t && alertas.push('Temperatura está fora da faixa ideal')
+            el.l.u != ideal.u && alertas.push('Umidade está fora da faixa ideal')
+            el.l.us != ideal.us && alertas.push('Umidade do solo está fora da faixa ideal')
+            el.l.c != ideal.c && alertas.push('Nível de Chuva está fora da faixa ideal')
+            el.l.l != ideal.l && alertas.push('Nível de Luz está fora da faixa ideal')
+        })
+        // value.telemetry[0].l.t != ideal.t && alertas.push('Temperatura está fora da faixa ideal')
+        // value.telemetry[0].l.u != ideal.u && alertas.push('Umidade está fora da faixa ideal')
+        // value.telemetry[0].l.us != ideal.us && alertas.push('Umidade do solo está fora da faixa ideal')
+        // value.telemetry[0].l.c != ideal.c && alertas.push('Nível de Chuva está fora da faixa ideal')
+        // value.telemetry[0].l.l != ideal.l && alertas.push('Nível de Luz está fora da faixa ideal')
     } else {
-        console.log(value.telemetry[0].ler.temperatura != ideal.t && 'Temperatura está fora da faixa ideal')
-        console.log(value.telemetry[0].ler.umidade != ideal.u && 'Umidade está fora da faixa ideal')
-        console.log(value.telemetry[0].ler.umidade_do_solo != ideal.us && 'Umidade do solo está fora da faixa ideal')
-        console.log(value.telemetry[0].ler.chuva != ideal.c && 'Nível de Chuva está fora da faixa ideal')
-        console.log(value.telemetry[0].ler.luz != ideal.l && 'Nível de Luz está fora da faixa ideal')
+        value.telemetry.forEach(el => {
+            el.ler.temperatura != ideal.t && alertas.push('Temperatura está fora da faixa ideal')
+            el.ler.umidade != ideal.u && alertas.push('Umidade está fora da faixa ideal')
+            el.ler.umidade_do_solo != ideal.us && alertas.push('Umidade do solo está fora da faixa ideal')
+            el.ler.chuva != ideal.c && alertas.push('Nível de Chuva está fora da faixa ideal')
+            el.ler.luz != ideal.l && alertas.push('Nível de Luz está fora da faixa ideal')
+        })
+        // value.telemetry[0].ler.temperatura != ideal.t && alertas.push('Temperatura está fora da faixa ideal')
+        // value.telemetry[0].ler.umidade != ideal.u && alertas.push('Umidade está fora da faixa ideal')
+        // value.telemetry[0].ler.umidade_do_solo != ideal.us && alertas.push('Umidade do solo está fora da faixa ideal')
+        // value.telemetry[0].ler.chuva != ideal.c && alertas.push('Nível de Chuva está fora da faixa ideal')
+        // value.telemetry[0].ler.luz != ideal.l && alertas.push('Nível de Luz está fora da faixa ideal')
     }
 
-    console.log("CPU: ", cpu)
-    console.log("MEM RSS: ", mem)
-    console.log("Delay: ", new Date() - value.firstMsg)
-    console.log("Msgs Recebidas: ", totalMsg)
+    // console.log("CPU: ", cpu)
+    // console.log("MEM RSS: ", mem)
+    // console.log("Delay: ", new Date() - value.firstMsg)
+    // console.log("Msgs Recebidas: ", totalMsg)
+
+    console.log(cpu, mem, new Date() - value.firstMsg, totalMsg)
+    // const csv = `${cpu},${mem},${new Date() - value.firstMsg},${totalMsg}\n`
+    // try {
+    //   fs.appendFileSync(`./${type}.csv`, csv)
+    // } catch (err) {
+    //   console.error(err)
+    // }
 }
